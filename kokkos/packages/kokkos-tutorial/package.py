41c6dec0bc1d149ac27371b74b0881fe078d4766
jjwilke <jjwilke@sandia.gov>
2019-08-25 21:52:51 -0700

    kokkos cleanup, added tutorial support

--- kokkos/packages/kokkos-tutorial/package.py
+++ kokkos/packages/kokkos-tutorial/package.py
@@ -6,32 +6,25 @@
 from spack import *
 
 
-class Kokkos(CMakePackage):
+class KokkosTutorial(CMakePackage):
     """Kokkos implements a programming model in C++ for writing performance
     portable applications targeting all major HPC platforms."""
 
     homepage = "https://github.com/kokkos/kokkos"
-    url      = "https://github.com/kokkos/kokkos/archive/2.8.00.tar.gz"
     git      = "https://github.com/kokkos/kokkos.git"
 
-    version('cmake', branch='cmake-overhaul')
-    
-    devices_variants = {
+    version("cmake", branch="tutorial")
+
+    variants = {
      'cuda'                           : [False, 'Whether to build CUDA backend'],
      'openmp'                         : [ True, 'Whether to build OpenMP backend'],
      'pthread'                        : [False, 'Whether to build Pthread backend'],
      'rocm'                           : [False, 'Whether to build AMD ROCm backend'],
      'serial'                         : [ True, 'Whether to build serial backend'],
-    }
-
-    tpls_variants = {
-      'hpx'                            : [False, 'Whether to enable the HPX library'],
-      'hwloc'                          : [False, 'Whether to enable the HWLOC library'],
-      'libnuma'                        : [False, 'Whether to enable the LIBNUMA library'],
-      'memkind'                        : [False, 'Whether to enable the MEMKIND library'],
-    }
-
-    options_variants = {
+     'hpx'                            : [False, 'Whether to enable the HPX library'],
+     'hwloc'                          : [False, 'Whether to enable the HWLOC library'],
+     'libnuma'                        : [False, 'Whether to enable the LIBNUMA library'],
+     'memkind'                        : [False, 'Whether to enable the MEMKIND library'],
      'aggressive_vectorization'       : [False, 'Whether to aggressively vectorize loops'],
      'compiler_warnings'              : [False, 'Whether to print all compiler warnings'],
      'cuda_lambda'                    : [False, 'Whether to activate experimental laambda features'],
@@ -49,11 +42,6 @@ class Kokkos(CMakePackage):
      'profiling_load_print'           : [False, 'Whether to print information about which profiling tools gotloaded'],
      'qthread'                        : [False, 'Whether to enable the QTHREAD library'],
      'tests'                          : [False, 'Whether to build for tests'],
-    }
-
-    cuda_arches = ["kepler" "maxwell", "pascal", "volta", "turing"]
-
-    arch_variants = {
      'amdavx'                         : [False, 'Whether to optimize for the AMDAVX architecture'],
      'armv80'                         : [False, 'Whether to optimize for the ARMV80 architecture'],
      'armv81'                         : [False, 'Whether to optimize for the ARMV81 architecture'],
@@ -93,75 +81,17 @@ class Kokkos(CMakePackage):
      'wsm'                            : [False, 'Whether to optimize for the WSM architecture'],
     }
 
-    arch_values = list(arch_variants.keys())
-    allowed_arch_values = arch_values[:]
-    allowed_arch_values.append("none")
-    variant(
-      'arch',
-      default="none",
-      values=allowed_arch_values,
-      description='Set the architecture to optimize for'
-    )
+    depends_on("kokkos")
 
-    for arch in arch_values:
-      for cuda_arch in cuda_arches:
-        if cuda_arch in arch:
-          conflicts("+%s" % arch, when="~cuda", 
-                    msg="Must specify +cuda for CUDA backend to use GPU architecture %s" % arch)
-      dflt, desc = arch_variants[arch]
-      variant(arch, default=dflt, description=desc)
+    for var in variants:
+      dflt, desc = variants[var]
+      variant(var, default=dflt, description=desc)
+      depends_on("kokkos+%s" % var, when="+%s" % var)
+      depends_on("kokkos~%s" % var, when="~%s" % var)
 
-    devices_values = list(devices_variants.keys())
-    variant(
-      'devices',
-      default="serial",
-      values=devices_values,
-      description='Set the devices to optimize for'
-    )
-    for dev in devices_variants:
-      dflt, desc = devices_variants[dev]
-      variant(dev, default=dflt, description=desc)
-
-
-    options_values = list(options_variants.keys())
-    for opt in options_values:
-      if "cuda" in opt:
-        conflicts('+%s' % opt, when="~cuda", msg="Must enable CUDA to use %s" % opt)
-      dflt, desc = options_variants[opt]
-      variant(opt, default=dflt, description=desc)
-
-    tpls_values = list(tpls_variants.keys())
-    for tpl in tpls_values:
-      dflt, desc = tpls_variants[tpl]
-      variant(tpl, default=dflt, description=desc)
-      depends_on(tpl, when="+%s" % tpl)
-
-    def append_args(self, cmake_prefix, cmake_options, spack_options):
-      for opt in cmake_options:
-        enableStr = "+%s" % opt
-        if enableStr in self.spec:
-          spack_options.append("-DKokkos_%s_%s=ON" % (prefix,opt))
-        
-  
     def cmake_args(self):
       spec = self.spec
       options = []
-
-      if spec.variants["arch"].value:
-        if spec.variants["arch"].value != "none":
-          options.append("-DKokkos_ARCH=%s" % spec.variants["arch"].value)
-      if spec.variants["devices"].value:
-        options.append("-DKokkos_DEVICES=%s" % spec.variants["devices"].value)
-
-      self.append_args("ENABLE", self.devices_values,  options)
-      self.append_args("ENABLE", self.options_values, options)
-      self.append_args("ENABLE", self.tpls_values, options)
-      self.append_args("ARCH",   self.arch_values, options)
-
-      for tpl in self.tpls_values:
-        var = "+%s" % tpl
-        if var in self.spec:
-          options.append("-D%s_DIR=%s" % (tpl, spec[tpl].prefix))
-
+      options.append("-DKokkos_DIR=%s/lib/cmake/Kokkos" % spec["kokkos"].prefix)
       return options
 
